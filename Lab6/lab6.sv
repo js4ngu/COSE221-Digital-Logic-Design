80dc3aabeb0a3c2a91b6440a5392bf6fca29b833
Jongsang yoo <jamin0513@gmail.com>
2023-01-20 18:38:57 +0900

    lab6 in progress

--- Lab6/lab6.sv
+++ Lab6/lab6.sv
@@ -3,16 +3,17 @@ module lab6(
     input      [9:0]	SW,
 	output reg [31:0]	cnt,
     output reg       	flag,
-    output reg [9:0]    LEDG
+    output     [9:0]    LEDG
 );
 	parameter target = 20000000;
 	parameter targetTB = 20;
+	reg out = 1;
 	always @(posedge CLOCK_50_2) begin
-		if 	    (target > cnt) begin
+		if 	    (targetTB > cnt) begin
 			 cnt <= cnt + 1;
 			 flag <= 0;
 		end
-		else if (cnt >= target) begin
+		else if (cnt >= targetTB) begin
 			cnt  <= 0;
 			flag <= 1;
 		end
@@ -20,8 +21,13 @@ module lab6(
 
 	always @(posedge CLOCK_50_2) begin
 		if (SW[9] == 1) begin
-			LEDG <= 10'b1;
+			out <= 1;
 		end
-		if(flag) LEDG <= 1;
+		if(flag == 1) begin
+			if(1024 > out)out <= out <<< 1;
+			else if(out >= 1024) out <= 1;
+		end
 	end
+
+	assign LEDG = 0x3FF;
 endmodule
